84b2f105d575bcebaa13d0412fd458b87b356258
Beellee <halley1986n@gmail.com>
2022-06-21 14:50:18 +0200

    added functions to hide and show the first extra info elements depending on the title + created a general formula for the subfolders background change (comented) +  added specific formula for the backround of the first extra

--- university.js
+++ university.js
@@ -1,22 +1,21 @@
-//selectors for the titles part 
+//change the background color of the tiltes part 
+//+ show the first extra info
 let titlesListener = document.querySelector(".titles")
 let titleElements = titlesListener.querySelectorAll("h4")
-// selectos for the firstExtraInfo part 
-let firstExtraListener=document.querySelector(".firstExtraInfo")
-let firstExtraElements = firstExtraListener.querySelectorAll("p")
-// selectors for the secondExtraInfo part 
-let secondExtraListener=document.querySelector(".secondExtraInfo")
-let secondExtraElements = secondExtraListener.querySelectorAll("p")
-
+let firstHide = document.querySelector("#firstHide")
 titlesListener.addEventListener("click", function(title){
   if(title.target.nodeName == "H4"){
     titleElements.forEach(h=>{
       h.style.backgroundColor="transparent";
     });
     title.target.style.backgroundColor = "#c6c4c3" 
+    firstHide.style.display="block"
   }
 })
-firstExtraListener.addEventListener("click", function(title){
+//background of the first extra elements
+let firstExtraElements = firstHide.querySelectorAll("p")
+firstHide.addEventListener("click", function(title){
+  console.log(firstExtraElements)
   if(title.target.nodeName == "P"){
     firstExtraElements.forEach(p=>{
       p.style.backgroundColor="transparent";
@@ -26,9 +25,15 @@ firstExtraListener.addEventListener("click", function(title){
     title.target.style.color= "#ffffff"
   }
 })
-secondExtraListener.addEventListener("click", function(title){
+// change the background of the subfolders
+/*
+let subfolders=document.querySelector(".subfolders")
+let extraElements = subfolders.querySelectorAll("p")
+
+subfolders.addEventListener("click", function(title){
+  console.log(extraElements)
   if(title.target.nodeName == "P"){
-    secondExtraElements.forEach(p=>{
+    extraElements.forEach(p=>{
       p.style.backgroundColor="transparent";
       p.style.color="#000000"
     });
@@ -36,3 +41,22 @@ secondExtraListener.addEventListener("click", function(title){
     title.target.style.color= "#ffffff"
   }
 })
+*/
+
+
+// FUNCTIONS TO HIDE AND SHOW THE SPECIFIC FOLDERS
+let subjects = document.querySelector("#subjects");
+function showSubjects(){
+  hideAll()
+  subjects.style.display = "block"
+}
+let info = document.querySelector("#info");
+function showInfo(){
+  hideAll()
+  info.style.display = "block"
+}
+function hideAll(){
+  subjects.style.display = "none"
+  info.style.display = "none"
+
+}
